c6ec4f67b767f57e06e1d4de3f01b33ee9b9fea0
Lalo (Edward) <67384140+lalohamb@users.noreply.github.com>
2025-04-23 10:47:18 -0500

    cleaning

--- project-bolt-sb1-wbn8dmur (HoodCoin Crowdsale Web3 Bronwn Page)/project/src/components/Crowdsale/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CrowdsaleInfo } from './CrowdsaleInfo';
-export { default as ContributeForm } from './ContributeForm';
-export { default as ContributorInfo } from './ContributorInfo';
